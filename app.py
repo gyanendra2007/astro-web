7d2bdbd779846780f3c8e8662a040d231eaeb830
gyanendra2007 <141428376+gyanendra2007@users.noreply.github.com>
2024-12-20 09:40:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,15 @@ from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 def generate_square_birth_chart(planets):
     # Create the figure and axis
     fig, ax = plt.subplots(figsize=(6, 6))
